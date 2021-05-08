8416d34d908a24a5d108b18f56c1e80f3f9ca849
tommyswang <tommyswang@gmail.com>
2021-05-08 11:48:27 -0400

    Created user authentication and add note

--- website/__init__.py
+++ website/__init__.py
@@ -1,27 +1,43 @@
-from os import environ, path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path, environ
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = str(environ.get('DB_NAME'))
 
+
 def create_app():
-  app = Flask(__name__)
-  app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-  db.init_app(app)
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
-  from .views import views
-  from .auth import auth
+    from .views import views
+    from .auth import auth
 
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-  from .models import User, Note
-  create_database(app)
-  return app
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
 
 def create_database(app):
-  if not path.exists('website/' + DB_NAME):
-    db.create_all(app=app)
-    print('Created Database!')
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
+
+
