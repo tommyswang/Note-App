3fda0ec89de7effbca099da56da0ae70b52db3c1
tommyswang <tommyswang@gmail.com>
2021-05-08 11:18:38 -0400

    Created DB schema for User and Notes tables

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
+from os import environ, path
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = str(environ.get('DB_NAME'))
 
 def create_app():
   app = Flask(__name__)
-  app.config['SECRET_KEY'] = ''
+  app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
 
   from .views import views
   from .auth import auth
@@ -10,4 +17,11 @@ def create_app():
   app.register_blueprint(views, url_prefix='/')
   app.register_blueprint(auth, url_prefix='/')
 
+  from .models import User, Note
+  create_database(app)
   return app
+
+def create_database(app):
+  if not path.exists('website/' + DB_NAME):
+    db.create_all(app=app)
+    print('Created Database!')
